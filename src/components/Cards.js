fc434fe05476014e961787bb2d77b5048946e02d
Emil Abdulxalıqov <emilabdulxaliqov@Emils-MacBook-Air.local>
2021-10-24 15:42:20 +0400

    change something

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -14,12 +14,14 @@ function Cards() {
               text="Explore the hidden waterfall deep inside the Amazon jungle"
               label="Adventure"
               path="/services"
+              alt="something"
             />
             <CardsItem
               src="https://github.com/briancodex/react-website-v1/blob/master/public/images/img-2.jpg?raw=true"
               text="Travel through the Islands of Bali in a Private Cruis"
               label="Luxury"
               path="/services"
+              alt="something"
             />
           </ul>
           <ul class="cards__items">
@@ -28,18 +30,21 @@ function Cards() {
               text="Set Sail in the Atlantic Ocean"
               label="Mystery"
               path="/services"
+              alt="something"
             />
             <CardsItem
               src="https://github.com/briancodex/react-website-v1/blob/master/public/images/img-4.jpg?raw=true"
               text="Experience Football on Top of the Himilayan Mountains"
               label="Adventure"
               path="/services"
+              alt="something"
             />
             <CardsItem
               src="https://github.com/briancodex/react-website-v1/blob/master/public/images/img-8.jpg?raw=true"
               text="Ride through the Sahara Desert on a guided camel tour "
               label="Adrenaline"
               path="/services"
+              alt="something"
             />
           </ul>
         </div>
